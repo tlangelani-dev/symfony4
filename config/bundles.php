ba1e1a80365eb66d6c20a313beab9ad2e08294ea
Tlangelani Mboweni <tlangelanimb@miway.co.za>
2017-12-12 10:33:18 +0200

    add logger

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class LuckyController extends Controller
 {
-    public function number()
+    public function number(LoggerInterface $logger)
     {
+        $logger->info('You requested a new number.');
         return $this->render('lucky/number.html.twig', [
             'number' => mt_rand(1, 100)
         ]);
